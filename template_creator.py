5a5d04bb761a01b517d698f1a5fc9978476dc690
Vinoth kumar A <bluerail@outlook.com>
2014-07-02 22:33:58 -0400

    included layout and home in templates

--- template_creator.py
+++ template_creator.py
@@ -7,23 +7,27 @@ project = raw_input("Enter the Project name")
 app_path = os.path.dirname(os.path.abspath(__file__)) + "\%s" % (project,)
 pkg_path = app_path + "\%s" % (project,)
 static = pkg_path + "\static"
+templates = pkg_path + "\templates"
 if not os.path.exists(app_path):
     os.makedirs(app_path)
     os.makedirs(pkg_path)
     os.makedirs(static)
-    os.makedirs(pkg_path + "\\templates")
+    os.makedirs(templates)
 
+#Create Runserver.py file
 with open(app_path + "\\runserver.py", 'w') as rserver:
     rserver.write(
 """from %s import app
 app.run(debug=True)""" % (project,))
 
+#Create __init__.py file inside the application package
 with open(pkg_path + "\\__init__.py", 'w') as init:
     init.write(
 """from flask import Flask
 app = Flask(__name__)
 import %s.views""" % (project))
-    
+
+#Create views.py as a placeholder py file 
 with open(pkg_path + "\\views.py", 'w') as views:
     views.write(
 """from %s import app
@@ -31,6 +35,33 @@ with open(pkg_path + "\\views.py", 'w') as views:
 def index():
     return 'Hello World!'""" % (project))
 
+#creating template files
+with open(templates + "\\layout.html") as lout:
+          lout.write(
+"""
+<!DOCTYPE html>
+<html>
+<head>
+Enter heading here
+</head>
+<body>
+<p> Some body content here...</p>
+</body>
+</html>""")
+
+with open(templates + "\\home.html") as home:
+          home.write(
+"""
+<!DOCTYPE html>
+<html>
+<head>
+Enter heading here
+</head>
+<body>
+<p> Some body content here...</p>
+</body>
+</html>""")    
 
 
-print "Project Structure successfully created"
+
+print "All Done"
